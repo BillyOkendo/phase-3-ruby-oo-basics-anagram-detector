bc688bd043f8e531c760ff9d9f583a5f942bfb39
Billy Otieno Okendo <billyotieno7@gmail.com>
2022-08-19 00:00:55 +0300

    defined Amagram

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :test
+
+    def initialize(word)
+      @test = word 
+    end   
+
+    def match(array)
+      array.select {|y| y.split("").sort == @test.split("").sort}
+    end   
+
+end
